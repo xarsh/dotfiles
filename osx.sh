fb17e8eaa818a93db393ecfed0b77cde646565ed
Takao Naito <xarsh@outlook.com>
2026-03-14 20:43:20 +0900

    Clean up osx.sh, add Finder sidebar and screenshot settings
    - Fix inconsistent -boolean/-bool and domain name casing
    - New Finder windows open Desktop instead of Recents
    - Hide Recents, iCloud Drive from sidebar
    - Disable shadow in screenshots
    - Add killall Finder to apply Finder settings

--- osx.sh
+++ osx.sh
@@ -7,11 +7,11 @@ sudo nvram StartupMute=%01
 defaults write com.apple.finder WarnOnEmptyTrash -bool false
 
 # Show only running app icons on the Dock
-defaults write com.apple.dock static-only -boolean true
+defaults write com.apple.dock static-only -bool true
 
 # Hide Dock automatically without any delay
 defaults write com.apple.dock autohide -bool true
-defaults write com.apple.Dock autohide-delay -float 0
+defaults write com.apple.dock autohide-delay -float 0
 
 # Set the icon size of Dock items to 64 pixels
 defaults write com.apple.dock tilesize -int 64
@@ -80,11 +80,24 @@ sudo pmset -c displaysleep 0
 
 
 # Finder
+# New Finder windows show Desktop instead of Recents
+defaults write com.apple.finder NewWindowTarget -string "PfDe"
+defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}/Desktop/"
+
+# Hide Recents from sidebar
+defaults write com.apple.finder ShowRecentTags -bool false
+
+# Hide iCloud Drive from sidebar
+defaults write com.apple.finder SidebarShowingiCloudDesktop -bool false
+
+# Hide Shared from sidebar (requires manually removing from Finder preferences)
+# Note: AirDrop cannot be hidden via defaults, must be done manually in Finder > Settings > Sidebar
+
 # Show all filename extensions
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 
 # Show hidden files
-defaults write com.apple.Finder AppleShowAllFiles -bool true
+defaults write com.apple.finder AppleShowAllFiles -bool true
 
 # Show status bar
 defaults write com.apple.finder ShowStatusBar -bool true
@@ -102,6 +115,9 @@ chflags nohidden ~/Library/
 defaults write com.apple.finder FXInfoPanesExpanded -dict OpenWith -bool true
 
 
+# Disable shadow in screenshots
+defaults write com.apple.screencapture disable-shadow -bool true
+
 # Set VLC as default for common video/audio formats
 for ext in avi flv mkv mov mp4 m4v mpg mpeg webm wmv ts \
            mp3 flac aac ogg wav m4a wma; do
@@ -111,3 +127,4 @@ done
 
 # Apply all confs
 killall Dock
+killall Finder
